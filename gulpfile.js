5fbb812faa3299488dce2d374bd3647ddd6c0713
yapcheahshen <yapcheahshen@gmail.com>
2014-11-02 20:13:36 +0800

    for mac

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp=require('../gulpfile-app.js');
 
 var spawn=require('child_process').spawn;
 gulp.afterbuild=function() {
-	var adbcommand="push build.js /sdcard/ksanagap/home/";
+	var adbcommand="push build.js /sdcard/accelon/yinshun/";
 	console.log("adb "+adbcommand);
 	var command=spawn("adb",adbcommand.split(" "));
 	command.stderr.pipe(process.stdout);
